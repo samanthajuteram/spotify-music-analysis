301bbfd733edd82e9f73bdc91d45fbc84fb44001
Samantha Juteram <157514377+samanthajuteram@users.noreply.github.com>
2024-06-06 12:21:06 -0400

    Update 04_modeling_and_prediction.R

--- scripts/04_modeling_and_prediction.R
+++ scripts/04_modeling_and_prediction.R
@@ -14,16 +14,17 @@ balanced_data <- balanced_data$data
 
 
 #SPLITTING DATASET INTO TRAIN AND TEST
-
 #install.packages("caTools", dep = T)
 library(caTools)
-
 
 split <- sample.split(balanced_data$Stream_cat, SplitRatio = 0.75)
 training_set <- subset(balanced_data, split == TRUE)
 test_set <- subset(balanced_data, split == FALSE)
 
 #RIDGE REGRESSION----
+
+#install.packages("glmnet", dep = T)
+library(glmnet)
 
 x <- model.matrix(Stream_cat ~ Loudness + Danceability * Instrumentalness , training_set)[,-1]
 # Convert the outcome (class) to a numerical variable
@@ -34,32 +35,32 @@ cv.ridge <- cv.glmnet(x, y, alpha = 0, family = "binomial")
 # Fit the final model on the training data
 model <- glmnet(x, y, alpha = 0, family = "binomial",
                 lambda = cv.ridge$lambda.min)
+
 # Display regression coefficients
 coef(model)
+
 # Make predictions on the test data
 x.test <- model.matrix(Stream_cat ~ Loudness + Energy * Instrumentalness , test_set)[,-1]
 probabilities <- model %>% predict(newx = x.test)
 predicted.classes <- ifelse(probabilities > 0.5, "High", "Low")
+
 # Model accuracy
 observed.classes <- test_set$Stream_cat
 mean(predicted.classes == observed.classes)
 
 predicted.classes <- as.factor(predicted.classes)
 observed.classes <- as.factor(observed.classes)
-
 levels(predicted.classes) <- levels(observed.classes) <- c("Low", "High")
 
+#install.packages("caret", dep = T)
 library(caret)
 
 confusionMatrix(predicted.classes, observed.classes)
 
 #SVM----
 
+#install.packages("e1071", dep = T)
 library(e1071)
-library(caTools)
-split <- sample.split(cleaned_spotify_youtube_data$Stream_cat, SplitRatio = 0.75)
-training_set <- subset(cleaned_spotify_youtube_data, split == TRUE)
-test_set <- subset(cleaned_spotify_youtube_data, split == FALSE)
 
 model_svm = svm(formula = Stream_cat ~ Loudness + Energy * Instrumentalness ,
                 data = training_set,
@@ -71,7 +72,6 @@ model_svm = svm(formula = Stream_cat ~ Loudness + Energy * Instrumentalness ,
 predictions <- predict(classifierR, newdata=test_set)
 table(predictions, actual = test_set$Stream_cat)
 
-
 predictions <- predict(classifierR, newdata=test_set)
 
 # Calculate accuracy
@@ -82,8 +82,8 @@ print(paste("Accuracy:", round(accuracy, 4)))
 
 #RANDOM FOREST----
 
+#install.packages("randomForest", dep = T)
 library(randomForest)
-
 
 model_rf <- randomForest(Stream_cat ~ Loudness + Energy * Instrumentalness,
                          data = training_set,
@@ -98,7 +98,6 @@ model_rf <- randomForest(Stream_cat ~ Loudness + Energy * Instrumentalness,
 predictions_rf <- predict(model_rf, newdata = test_set)
 table(predictions_rf, actual = test_set$Stream_cat)
 
-
 # Accuracy
 accuracy_rf <- mean(predictions_rf == test_set$Stream_cat)
 print(paste("Random Forest Accuracy :", round(accuracy_rf, 4)))
@@ -106,15 +105,12 @@ print(paste("Random Forest Accuracy :", round(accuracy_rf, 4)))
 #FURTHER EVALUATION OF RANDOM FOREST MODEL
 
 #install.packages("pROC",dep = T)
-
-
 library(pROC)
 
 # Calculate the F1 score
 precision <- sum(predictions_rf == "High" & test_set$Stream_cat == "High") / sum(predictions_rf == "High")
 recall <- sum(predictions_rf == "High" & test_set$Stream_cat == "High") / sum(test_set$Stream_cat == "High")
 f1 <- 2 * (precision * recall) / (precision + recall)
-
 
 # Print the F1 score
 print(paste("F1 Score:", round(f1, 4)))
@@ -132,14 +128,11 @@ auc_score
 
 # Make predictions on the entire dataset
 predictions_data <- predict(model_rf, newdata = cleaned_spotify_youtube_data)
-
-library(caret)
 confusionMatrix(predictions_data, reference = cleaned_spotify_youtube_data$Stream_cat)
 
 #Accuracy
 accuracy_rf <- mean(predictions_data == cleaned_spotify_youtube_data$Stream_cat)
 print(paste("Random Forest Accuracy (Entire Dataset):", round(accuracy_rf, 4)))
-
 
 #Evaluating model
 importance(model_rf)
